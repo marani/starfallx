bd96c5de15db265b1a240598be2a790c18d325dd
Pierre <dungdung1@e.ntu.edu.sg>
2014-06-04 10:24:33 +0700

    UI Improve for build page

--- app/scripts/controllers/build.js
+++ app/scripts/controllers/build.js
@@ -5,4 +5,39 @@ angular.module('starfallxApp')
         // $http.get('/api/awesomeThings').success(function(awesomeThings) {
         //     $scope.awesomeThings = awesomeThings;
         // });
+    })
+    .controller('PrefCtrl', function($scope){
+        // $scope.courses = ['IM4791', 'CE4015'];
+        $scope.autypes = ['Core', 'UE'];
+        $scope.autypes = {
+            'CORE': true,
+            'UE': false,
+            'PE': true
+        }
+        $scope.newCourse = {
+            name: '',
+            autypes: {
+            'CORE': false,
+            'UE': false,
+            'PE': false
+            }
+        }
+        $scope.toggleAUType = function(course, autype) {
+            if (course.autypes) {
+                course.autypes[autype] = !course.autypes[autype];
+            } else 
+                $scope.autypes[autype] = !$scope.autypes[autype];
+        }
+        $scope.addCourse = function() {
+            console.log('adding new course...');
+        }
+        // $scope.indexes = ['36788', '36789', '36790', 'Any Index'];
+        // $scope.chosenIndex = $scope.indexes[0];
+        // $scope.pickIndex = function(index) {
+        //     $scope.chosenIndex = index;
+        //     $scope.status.isopen = false;
+        // }
+        // $scope.status = {
+        //     isopen: false
+        // }
     });
