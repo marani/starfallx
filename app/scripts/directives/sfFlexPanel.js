05e601c36858746060fd5d35b1c1e76af3ff8dcc
Pierre <dungdung1@e.ntu.edu.sg>
2014-07-23 10:11:44 +0800

    Rework table headers gradient, add Firefox Support.

--- app/scripts/directives/sfFlexPanel.js
+++ app/scripts/directives/sfFlexPanel.js
@@ -1,74 +1,7 @@
-/* Preference Panel directives, solving special layout problems
- * manipulate: all course-pref-col elements
- * reuquirement: see paper notes
+/* 
+ * Perfect Scrollbar Wrapper directives,
  */
- /*
-angular.module('starfallxApp')
-    .directive('prefPanel', function($window) {
-        return {
-            // scope: true,
-            restrict: 'A',
-            link: function(scope, el, attrs) {
-                var floatCtn = $('.ctn-btn-float');
-                var fixedCtn = $('.ctn-btn-fixed');
-                var flexPanel = $('.flexPanel');
-                var alertCtn = $('.ctn-alert');
-                var bgTop = $('.border-grad-top');
-                var bgBtm = $('.border-grad-btm');
-                showFloat();
-                function showFloat() {
-                    fixedCtn.css('visibility', 'hidden');
-                    floatCtn.css('display', 'block');
-                    flexPanel.css('margin-bottom', '0px');
-                    bgTop.css('visibility', 'hidden');
-                    bgBtm.css('visibility', 'hidden');
-                }
-                function showFixed() {
-                    fixedCtn.css('visibility', '');
-                    floatCtn.css('display', 'none');
-                    flexPanel.css('margin-bottom', fixedCtn.css('height'));
-                    bgTop.css('top', flexPanel.css('margin-top'));
-                    bgBtm.css('bottom', flexPanel.css('margin-bottom'));
-                    bgTop.css('visibility', '');
-                    bgBtm.css('visibility', '');
-                }
-                function updateLayout() {
-                    flexPanel.css('margin-top', 
-                        (parseFloat(alertCtn.css('top').replace('px', '')) + parseFloat(alertCtn.height())) + 'px');
 
-                    if (flexPanel.get(0).scrollHeight > flexPanel.height()) 
-                        showFixed();
-                    else
-                        showFloat();
-                    scope.$emit('updateScroll', {});
-                }
-                flexPanel.on('DOMSubtreeModified', function() {
-                    // console.log('sub tree modified');
-                    updateLayout();
-                });
-                $($window).on('resize', function() {
-                    updateLayout();
-                });
-                scope.$on('layoutChange', function() {
-                    // console.log('layout update from directive');
-                    updateLayout();
-                });
-            }
-        }
-    })
-    .directive('showAndEmit', function() {
-        return {
-            link: function(scope, el, attrs) {
-                scope.$watch(attrs.showAndEmit, function(value) {
-                    value != value;
-                    // $animator(scope, attrs)[value ? "show" : "hide"]
-                    el.css('display', value ? 'block' : 'none');
-                    scope.$emit('layoutChange', {});
-                })
-            }
-        }
-    });
-*/
 angular.module('starfallxApp')
     .directive('sfPerfectScrollbar', function($window, $parse){
         return {
@@ -123,20 +56,25 @@ angular.module('starfallxApp')
             }
         }
     })
+
+/*
+ * Flexlayout directives, solving special layout problems
+ * manipulate: all course-pref-col elements
+ */
     .directive('resultPanel', function($window) {
         return {
             restrict: 'A',
             link: function($scope, $elem, attrs) {
                 var scrollPanel = $elem.find('.scroll-panel');
-                var bgTop = $elem.find('.border-grad-top');
+                // var bgTop = $elem.find('.border-grad-top');
                 var bgBtm = $elem.find('.border-grad-btm');
                 showFloat();
                 function showFloat() {
-                    bgTop.css('visibility', 'hidden');
+                    // bgTop.css('visibility', 'hidden');
                     bgBtm.css('visibility', 'hidden');
                 }
                 function showFixed() {
-                    bgTop.css('top', scrollPanel.css('margin-top'));
+                    // bgTop.css('top', scrollPanel.css('margin-top'));
                     bgBtm.css('bottom', scrollPanel.css('margin-bottom'));
                     // bgTop.css('visibility', '');
                     // bgBtm.css('visibility', '');
